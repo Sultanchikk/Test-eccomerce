c053efb2329bb7be9f2d040d9cdeddf6871d4af0
unknown <oibeka50@gmail.com>
2023-08-29 18:57:17 +0600

    fut

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,23 @@
+
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='block_futer'>
+            <ul>
+                <h3>о нас </h3>
+                <li>о компании </li>
+                <li>контакты</li>
+                <li>FAQ</li>
+            </ul>
+            <ul>
+                <li>заказы</li>
+                <li>список желаний</li>
+                <li>войти</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
